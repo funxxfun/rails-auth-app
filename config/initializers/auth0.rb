eb2ef97bb2d367a721df6208688b20b71ffd76a6
funxxfun <23shirasawa@gmail.com>
2024-05-08 09:55:16 +0900

    Auth０の設定を変更

--- config/initializers/auth0.rb
+++ config/initializers/auth0.rb
@@ -4,8 +4,8 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider(
     :auth0,
     # AUTH0_CONFIG['auth0_client_id'],
-    AUTH0_CONFIG['auth0_client_secret'],
-    AUTH0_CONFIG['auth0_domain'],
+    # AUTH0_CONFIG['auth0_client_secret'],
+    # AUTH0_CONFIG['auth0_domain'],
     callback_path: '/auth/auth0/callback',
     authorize_params: {
       scope: 'openid profile'
